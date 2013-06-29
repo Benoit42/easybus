48f3f3812ee81eed24c2174333b3fc9ddaa87628
Benoît <benoit.leroux@me.com>
2013-06-29 21:28:15 +0200

    Gestion des données Keolis en CoreData

---
+++ EasyBusTests/StaticDataManagerTest.m
@@ -0,0 +1,13 @@
+//
+//  StaticDataManagerTest.m
+//  EasyBus
+//
+//  Created by Benoit on 23/06/13.
+//  Copyright (c) 2013 Benoit. All rights reserved.
+//
+
+#import "StaticDataManagerTest.h"
+
+@implementation StaticDataManagerTest
+
+@end
