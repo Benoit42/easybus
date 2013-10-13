a9dd8c0bb90c8e7c6955158c9240ccff0190601d
Benoît <benoit.leroux@me.com>
2013-10-13 10:28:46 +0200

    Optimisation du chargement (hash de RouteStop.m)

--- EasyBus/RouteStop.m
+++ EasyBus/RouteStop.m
@@ -13,11 +13,26 @@
 @synthesize routeId, directionId, stopId, stopSequence;
 
 - (BOOL)isEqual:(RouteStop*)other {
-    return [self.routeId isEqualToString:other.routeId] && [self.directionId isEqualToString:other.directionId] && [self.stopId isEqualToString:other.stopId] && [self.stopSequence isEqual:other.stopSequence];
+    if (other == self) {
+        return YES;
+    }
+    else if (!other || ![other isKindOfClass:[self class]]) {
+        return NO;
+    }
+    else {
+        return [self.routeId isEqualToString:other.routeId] && [self.directionId isEqualToString:other.directionId] && [self.stopId isEqualToString:other.stopId] && [self.stopSequence isEqual:other.stopSequence];
+    }
 }
 
 - (NSUInteger)hash {
-    return [[[[self.routeId stringByAppendingString:self.directionId] stringByAppendingString:self.stopId] stringByAppendingString:[self.stopSequence stringValue]] hash] ;
+    // See http://stackoverflow.com/questions/254281/best-practices-for-overriding-isequal-and-hash/254380#254380
+    NSUInteger prime = 31;
+    NSUInteger result = 1;
+    result = prime * result + [self.routeId hash];
+    result = prime * result + [self.directionId hash];
+    result = prime * result + [self.stopId hash];
+    result = prime * result + [self.stopSequence hash];
+    return result;
 }
 
 @end
