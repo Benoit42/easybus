a0f0b3c96abbf24fe7f8b535ef793b119ada539a
Benoît Le Roux <benoit.leroux@orange.fr>
2014-02-08 18:56:54 +0100

    Remplacement de StaticDataManager par une catégorie sur NSMAnagedObjectContext

---
+++ EasyBus/NSManagedObjectContext+Network.h
@@ -0,0 +1,13 @@
+//
+//  NSManagedObjectContext+EasyBus.h
+//  EasyBus
+//
+//  Created by Benoît on 08/02/2014.
+//  Copyright (c) 2014 Benoit. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSManagedObjectContext (EasyBus)
+
+@end
---
+++ EasyBus/NSManagedObjectContext+Network.m
@@ -0,0 +1,13 @@
+//
+//  NSManagedObjectContext+EasyBus.m
+//  EasyBus
+//
+//  Created by Benoît on 08/02/2014.
+//  Copyright (c) 2014 Benoit. All rights reserved.
+//
+
+#import "NSManagedObjectContext+EasyBus.h"
+
+@implementation NSManagedObjectContext (EasyBus)
+
+@end
