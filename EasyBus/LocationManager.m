978c1c1e134f7cbf9c529ff2150c147fa7ddfc40
Benoît Le Roux <benoit.leroux@orange.fr>
2014-03-10 13:20:21 +0100

    Modification du calcul de la géolocalisation

--- EasyBus/LocationManager.m
+++ EasyBus/LocationManager.m
@@ -63,17 +63,9 @@ NSString* const locationFoundNotification = @"locationFoundNotification";
     // If it's a relatively recent event, turn off updates to save power
     CLLocation* location = [locations lastObject];
 
-    // test the age of the location measurement to determine if the measurement is cached
-    // in most cases you will not want to rely on cached measurements
-    NSTimeInterval locationAge = -[location.timestamp timeIntervalSinceNow];
-    NSLog(@"Geo-location age : %f", locationAge);
-//    if (locationAge > 15.0) return;
-
     // test that the horizontal accuracy does not indicate an invalid measurement
-    if (location.horizontalAccuracy < 0) return;
-
-    if (location.horizontalAccuracy <= 300) {
-        NSLog(@"Geo-location succeeded");
+    if (location.horizontalAccuracy > 0) {
+        NSLog(@"Geo-location succeeded : accuracy = %f", location.horizontalAccuracy);
 
         // If the event is recent and accurate, do something with it.
         self.currentLocation = location;
